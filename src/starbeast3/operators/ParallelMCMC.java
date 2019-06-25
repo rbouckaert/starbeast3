40fcc390010ebd06e091dc65b5f3e7b8428fc963
Remco Bouckaert <higgs.ml@gmail.com>
2019-06-25 19:45:43 +1200

    add ParallelMCMC implementations

---
+++ src/starbeast3/operators/ParallelMCMC.java
@@ -0,0 +1,312 @@
+package starbeast3.operators;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import beast.core.Description;
+import beast.core.Distribution;
+import beast.core.Input.Validate;
+import beast.core.MCMC;
+import beast.core.Operator;
+import beast.core.State;
+import beast.core.StateNode;
+import beast.core.util.CompoundDistribution;
+import beast.core.util.Evaluator;
+import beast.core.util.Log;
+import beast.util.Randomizer;
+
+@Description("Implements MCMC without logging, or resume and suppresses all screen output. Used for ParallelMCMCOperator.")
+public class ParallelMCMC extends MCMC {
+	
+	public ParallelMCMC() {
+		loggersInput.setRule(Validate.OPTIONAL);
+	}
+
+    private static final boolean printDebugInfo = false;
+
+    @Override
+    public void initAndValidate() {
+        operatorSchedule = operatorScheduleInput.get();
+        for (final Operator op : operatorsInput.get()) {
+            operatorSchedule.addOperator(op);
+        }
+
+        if (sampleFromPriorInput.get()) {
+            // remove beastObject with id likelihood from posterior, if it is a CompoundDistribution
+            if (posteriorInput.get() instanceof CompoundDistribution) {
+                final CompoundDistribution posterior = (CompoundDistribution) posteriorInput.get();
+                final List<Distribution> distrs = posterior.pDistributions.get();
+                final int distrCount = distrs.size();
+                for (int i = 0; i < distrCount; i++) {
+                    final Distribution distr = distrs.get(i);
+                    final String id = distr.getID();
+                    if (id != null && id.equals("likelihood")) {
+                        distrs.remove(distr);
+                        break;
+                    }
+                }
+                if (distrs.size() == distrCount) {
+                    throw new RuntimeException("Sample from prior flag is set, but distribution with id 'likelihood' is " +
+                            "not an input to posterior.");
+                }
+            } else {
+                throw new RuntimeException("Don't know how to sample from prior since posterior is not a compound distribution. " +
+                        "Suggestion: set sampleFromPrior flag to false.");
+            }
+        }
+
+        // State initialisation
+        final HashSet<StateNode> operatorStateNodes = new HashSet<>();
+        for (final Operator op : operatorsInput.get()) {
+            for (final StateNode stateNode : op.listStateNodes()) {
+                operatorStateNodes.add(stateNode);
+            }
+        }
+        if (startStateInput.get() != null) {
+            this.state = startStateInput.get();
+            if (storeEveryInput.get() > 0) {
+                this.state.m_storeEvery.setValue(storeEveryInput.get(), this.state);
+            }
+        } else {
+            // create state from scratch by collecting StateNode inputs from Operators
+            this.state = new State();
+            for (final StateNode stateNode : operatorStateNodes) {
+                this.state.stateNodeInput.setValue(stateNode, this.state);
+            }
+            this.state.m_storeEvery.setValue(storeEveryInput.get(), this.state);
+        }
+
+        // grab the interval for storing the state to file
+        if (storeEveryInput.get() > 0) {
+            storeEvery = storeEveryInput.get();
+        } else {
+            storeEvery = state.m_storeEvery.get();
+        }
+
+        this.state.initialise();
+        this.state.setPosterior(posteriorInput.get());
+
+        // sanity check: all operator state nodes should be in the state
+        final List<StateNode> stateNodes = this.state.stateNodeInput.get();
+        for (final Operator op : operatorsInput.get()) {
+            List<StateNode> nodes = op.listStateNodes();
+            if (nodes.size() == 0) {
+                    throw new RuntimeException("Operator " + op.getID() + " has no state nodes in the state. "
+                                    + "Each operator should operate on at least one estimated state node in the state. "
+                                    + "Remove the operator or add its statenode(s) to the state and/or set estimate='true'.");
+                    // otherwise the chain may hang without obvious reason
+            }
+	        for (final StateNode stateNode : op.listStateNodes()) {
+	            if (!stateNodes.contains(stateNode)) {
+	                throw new RuntimeException("Operator " + op.getID() + " has a statenode " + stateNode.getID() + " in its inputs that is missing from the state.");
+	            }
+	        }
+	    }
+    
+        // sanity check: at least one operator required to run MCMC
+        if (operatorsInput.get().size() == 0) {
+        	Log.warning.println("Warning: at least one operator required to run the MCMC properly, but none found.");
+        }
+        
+        // sanity check: all state nodes should be operated on
+        for (final StateNode stateNode : stateNodes) {
+            if (!operatorStateNodes.contains(stateNode)) {
+                Log.warning.println("Warning: state contains a node " + stateNode.getID() + " for which there is no operator.");
+            }
+        }
+        state.m_storeEvery.setValue(Integer.MAX_VALUE, state);
+
+        burnIn = 0;//burnInInput.get();
+        chainLength = chainLengthInput.get();
+        state.setEverythingDirty(true);
+        posterior = posteriorInput.get();
+    } // init
+    
+    
+    @Override
+    public void run() throws IOException, SAXException, ParserConfigurationException {
+        // set up state (again). Other beastObjects may have manipulated the
+        // StateNodes, e.g. set up bounds or dimensions
+        state.initAndValidate();
+        // also, initialise state with the file name to store and set-up whether to resume from file
+//        state.setStateFileName(stateFileName);
+//        operatorSchedule.setStateFileName(stateFileName);
+
+  
+        state.storeCalculationNodes();
+        
+        // do the sampling
+        logAlpha = 0;
+        debugFlag = Boolean.valueOf(System.getProperty("beast.debug"));
+
+        doLoop();
+    } // run;
+
+
+    /**
+     * main MCMC loop 
+     * @throws IOException *
+     */
+    protected void doLoop() throws IOException {
+        int corrections = 0;
+        final boolean isStochastic = posterior.isStochastic();
+        
+        if (burnIn > 0) {
+        	Log.warning.println("Please wait while BEAST takes " + burnIn + " pre-burnin samples");
+        }
+        for (long sampleNr = -burnIn; sampleNr <= chainLength; sampleNr++) {
+            final Operator operator = propagateState(sampleNr);
+
+            if (debugFlag && sampleNr % 3 == 0 || sampleNr % 10000 == 0) {
+                // check that the posterior is correctly calculated at every third
+                // sample, as long as we are in debug mode
+            	final double originalLogP = isStochastic ? posterior.getNonStochasticLogP() : oldLogLikelihood;
+                final double logLikelihood = isStochastic ? state.robustlyCalcNonStochasticPosterior(posterior) : state.robustlyCalcPosterior(posterior);
+                if (isTooDifferent(logLikelihood, originalLogP)) {
+                    reportLogLikelihoods(posterior, "");
+                    Log.err.println("At sample " + sampleNr + "\nLikelihood incorrectly calculated: " + originalLogP + " != " + logLikelihood
+                    		+ "(" + (originalLogP - logLikelihood) + ")"
+                            + " Operator: " + operator.getName());
+                }
+                if (sampleNr > NR_OF_DEBUG_SAMPLES * 3) {
+                    // switch off debug mode once a sufficient large sample is checked
+                    debugFlag = false;
+                    if (isTooDifferent(logLikelihood, originalLogP)) {
+                        // incorrect calculation outside debug period.
+                        // This happens infrequently enough that it should repair itself after a robust posterior calculation
+                        corrections++;
+                        if (corrections > 100) {
+                            // after 100 repairs, there must be something seriously wrong with the implementation
+                        	Log.err.println("Too many corrections. There is something seriously wrong that cannot be corrected");
+                            state.storeToFile(sampleNr);
+                            operatorSchedule.storeToFile();
+                            System.exit(1);
+                        }
+                        oldLogLikelihood = state.robustlyCalcPosterior(posterior);;
+                    }
+                } else {
+                    if (isTooDifferent(logLikelihood, originalLogP)) {
+                        // halt due to incorrect posterior during intial debug period
+                        state.storeToFile(sampleNr);
+                        operatorSchedule.storeToFile();
+                        System.exit(1);
+                    }
+                }
+            } else {
+                if (sampleNr >= 0) {
+                	operator.optimize(logAlpha);
+                }
+            }
+            callUserFunction(sampleNr);
+
+            if (posterior.getCurrentLogP() == Double.POSITIVE_INFINITY) {
+            	throw new RuntimeException("Encountered a positive infinite posterior. This is a sign there may be numeric instability in the model.");
+            }
+        }
+        if (corrections > 0) {
+        	Log.err.println("\n\nNB: " + corrections + " posterior calculation corrections were required. This analysis may not be valid!\n\n");
+        }
+    }
+
+    /**
+     * Perform a single MCMC propose+accept/reject step.
+     *
+     * @param sampleNr the index of the current MCMC step
+     * @return the selected {@link beast.core.Operator}
+     */
+    protected Operator propagateState(final long sampleNr) {
+        state.store(sampleNr);
+//            if (m_nStoreEvery > 0 && sample % m_nStoreEvery == 0 && sample > 0) {
+//                state.storeToFile(sample);
+//            	operatorSchedule.storeToFile();
+//            }
+
+        final Operator operator = operatorSchedule.selectOperator();
+
+        if (printDebugInfo) System.err.print("\n" + sampleNr + " " + operator.getName()+ ":");
+
+        final Distribution evaluatorDistribution = operator.getEvaluatorDistribution();
+        Evaluator evaluator = null;
+
+        if (evaluatorDistribution != null) {
+            evaluator = new Evaluator() {
+                @Override
+                public double evaluate() {
+                    double logP = 0.0;
+
+                    state.storeCalculationNodes();
+                    state.checkCalculationNodesDirtiness();
+
+                    try {
+                        logP = evaluatorDistribution.calculateLogP();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+
+                    state.restore();
+                    state.store(sampleNr);
+
+                    return logP;
+                }
+            };
+        }
+        final double logHastingsRatio = operator.proposal(evaluator);
+
+        if (logHastingsRatio != Double.NEGATIVE_INFINITY) {
+
+            if (operator.requiresStateInitialisation()) {
+                state.storeCalculationNodes();
+                state.checkCalculationNodesDirtiness();
+            }
+
+            newLogLikelihood = posterior.calculateLogP();
+
+            logAlpha = newLogLikelihood - oldLogLikelihood + logHastingsRatio; //CHECK HASTINGS
+            if (printDebugInfo) System.err.print(logAlpha + " " + newLogLikelihood + " " + oldLogLikelihood);
+
+            if (logAlpha >= 0 || Randomizer.nextDouble() < Math.exp(logAlpha)) {
+                // accept
+                oldLogLikelihood = newLogLikelihood;
+                state.acceptCalculationNodes();
+
+                if (sampleNr >= 0) {
+                    operator.accept();
+                }
+                if (printDebugInfo) System.err.print(" accept");
+            } else {
+                // reject
+                if (sampleNr >= 0) {
+                    operator.reject(newLogLikelihood == Double.NEGATIVE_INFINITY ? -1 : 0);
+                }
+                state.restore();
+                state.restoreCalculationNodes();
+                if (printDebugInfo) System.err.print(" reject");
+            }
+            state.setEverythingDirty(false);
+        } else {
+            // operation failed
+            if (sampleNr >= 0) {
+                operator.reject(-2);
+            }
+            state.restore();
+            if (!operator.requiresStateInitialisation()) {
+                state.setEverythingDirty(false);
+                state.restoreCalculationNodes();
+            }
+            if (printDebugInfo) System.err.print(" direct reject");
+        }
+        return operator;
+    }
+
+    private boolean isTooDifferent(double logLikelihood, double originalLogP) {
+    	//return Math.abs((logLikelihood - originalLogP)/originalLogP) > 1e-6;
+    	return Math.abs(logLikelihood - originalLogP) > 1e-6;
+	}
+
+}
---
+++ src/starbeast3/operators/ParallelMCMCOperator.java
@@ -0,0 +1,79 @@
+package starbeast3.operators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+
+import beast.app.BeastMCMC;
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.MCMC;
+import beast.core.Operator;
+import beast.core.util.Log;
+
+@Description("Run MCMC on different parts of the model in parallel before combining them in a single Gibbs move")
+public class ParallelMCMCOperator extends Operator {
+	final public Input<List<ParallelMCMC>> mcmcInput = new Input<>("mcmc", "mcmc specification on a small subset of parameters. "
+			+ "Each mcmc has to be on an independent part of parameter space, for instance, mutation rate moves "
+			+ "for individual partitions.", new ArrayList<>());
+    final public Input<Integer> maxNrOfThreadsInput = new Input<>("threads","maximum number of threads to use, if "
+    		+ "less than 1 the number of threads in BeastMCMC is used (default -1)", -1);
+
+    private ExecutorService exec;
+    private CountDownLatch countDown;
+    private List<ParallelMCMC> mcmcs;
+    
+	@Override
+	public void initAndValidate() {
+		mcmcs = mcmcInput.get();
+		int nrOfThreads = maxNrOfThreadsInput.get() > 0 ? 
+				Math.min(BeastMCMC.m_nThreads, maxNrOfThreadsInput.get()) : 
+				BeastMCMC.m_nThreads;
+	    exec = Executors.newFixedThreadPool(nrOfThreads);
+	}
+
+	@Override
+	public double proposal() {
+		proposeUsingThreads();
+		return Double.POSITIVE_INFINITY;
+	}
+
+    class CoreRunnable implements Runnable {
+        MCMC mcmc;
+
+        CoreRunnable(MCMC core) {
+        	mcmc = core;
+        }
+
+        @Override
+		public void run() {
+            try {
+            	mcmc.run();
+            } catch (Exception e) {
+                Log.err.println("Something went wrong in a calculation of " + mcmc.getID());
+                e.printStackTrace();
+                System.exit(1);
+            }
+            countDown.countDown();
+        }
+
+    } // CoreRunnable
+
+    private void proposeUsingThreads() {
+        try {
+
+            countDown = new CountDownLatch(mcmcs.size());
+            // kick off the threads
+            for (MCMC mcmc : mcmcs) {
+                CoreRunnable coreRunnable = new CoreRunnable(mcmc);
+                exec.execute(coreRunnable);
+            }
+            countDown.await();
+        } catch (RejectedExecutionException | InterruptedException e) {
+            Log.err.println("Stop using threads: " + e.getMessage());
+        }
+    }
+}
