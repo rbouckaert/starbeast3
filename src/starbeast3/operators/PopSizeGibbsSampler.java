6a2ec54abec31d2d4142332987988209c56bfc9e
Remco Bouckaert <higgs.ml@gmail.com>
2019-08-02 07:11:02 +1200

    enhance PopSizeGibbsSampler for coalescent constant population models

--- src/starbeast3/operators/PopSizeGibbsSampler.java
+++ src/starbeast3/operators/PopSizeGibbsSampler.java
@@ -11,7 +11,9 @@ import beast.core.Operator;
 import beast.core.Input.Validate;
 import beast.core.parameter.RealParameter;
 import beast.evolution.tree.TreeInterface;
+import beast.evolution.tree.coalescent.TreeIntervals;
 import beast.math.distributions.Gamma;
+import beast.math.distributions.InverseGamma;
 import starbeast3.GeneTreeForSpeciesTreeDistribution;
 import starbeast3.util.*;
 
@@ -20,10 +22,12 @@ public class PopSizeGibbsSampler extends Operator {
 	final public Input<RealParameter> popSizesInput = new Input<>("popSizes", "constant population size parameter, one dimension for each branch of the species tree", Validate.REQUIRED);
 	final public Input<Gamma> priorInput = new Input<>("gammaprior", "gamma distributed prior for population sizes", Validate.REQUIRED);
 	final public Input<List<GeneTreeForSpeciesTreeDistribution>> genePriorsInput = new Input<>("gene", "gene tree for species tree distribution for each of the genes", new ArrayList<>());
+	final public Input<TreeIntervals> treeIntervalsInput = new Input<>("intervals", "tree intervals for use with single tree -- should not be used if gene-attribute is used");
 
 	RealParameter popSizes, priorAlpha, priorBeta;
 	Gamma prior;
 	List<GeneTreeForSpeciesTreeDistribution> genePriors;
+	TreeIntervals treeIntervals;
 
 	// used to sample a gamma distribution
 	MyRandomizer myRandomizer = new MyRandomizer();
@@ -33,8 +37,15 @@ public class PopSizeGibbsSampler extends Operator {
 		popSizes = popSizesInput.get();
 		prior = priorInput.get();
 		genePriors = genePriorsInput.get();
+		treeIntervals = treeIntervalsInput.get();
+		priorAlpha = prior.alphaInput.get();
+		priorBeta = prior.betaInput.get();
+		if (treeIntervals != null) { 
+			return;
+		}
+		
 		if (genePriors.size() == 0) {
-			throw new IllegalArgumentException("At least one gene needs to be prodided");
+			throw new IllegalArgumentException("At least one gene needs to be prodided (or intervals specified)");
 		}
 		
 		TreeInterface speciesTree = genePriors.get(0).speciesTreeInput.get();
@@ -42,8 +53,6 @@ public class PopSizeGibbsSampler extends Operator {
 			throw new IllegalArgumentException("The dimension of the population size parameter (" + popSizes.getDimension()+ ") "
 					+ "should be equal to the number of branches in the species tree ( " + speciesTree.getNodeCount() + ")");
 		}
-		priorAlpha = prior.alphaInput.get();
-		priorBeta = prior.betaInput.get();
 	}
 	
 
@@ -69,6 +78,10 @@ public class PopSizeGibbsSampler extends Operator {
 	 * @return
 	 */
 	private double samplePopSize(int branch) {
+		if (treeIntervals != null) {
+			return constantCoalescentSample();
+		}
+		
 		double a = 0; // = sum_j k_{jb}
 		for (GeneTreeForSpeciesTreeDistribution prior : genePriors) {
 			a += prior.getCoalescentCount(branch);
@@ -112,5 +125,34 @@ public class PopSizeGibbsSampler extends Operator {
 		}
 		System.out.println(sum / n);
 	}
-*/	
+*/
+
+
+	private double constantCoalescentSample() {
+		double a = 0; // = sum_j k_{jb}
+		TreeInterface tree = treeIntervals.treeInput.get();
+		a = tree.getInternalNodeCount();
+		
+		double b = 0; // = sum_j 1/ploidy \sum_i c_jbi(2 choose (n_jb - i))
+
+		double c = 0;
+		double [] times = treeIntervals.getCoalescentTimes(null);
+		double nbj = tree.getLeafNodeCount(); //prior.getLineageCount(branch);
+		c += (times[0]) * (nbj) * (nbj - 1) / 2;
+		nbj--;
+		for (int i = 0; i < times.length - 1; i++) {
+			c += (times[i+1] - times[i]) * (nbj - i) * (nbj - i - 1) / 2;
+		}
+		double ploidy = 1; // prior.getPloidy();
+		c /= ploidy;
+		b += c;
+		
+		
+		double alpha = prior.alphaInput.get().getValue() + a;
+		double beta = prior.betaInput.get().getValue() + b;
+		
+		GammaDistribution g = new GammaDistribution(myRandomizer, alpha, 1.0/beta, GammaDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY);
+		double newN = 1.0/g.sample();
+		return newN;
+	}	
 }
