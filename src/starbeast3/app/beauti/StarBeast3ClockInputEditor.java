7f21cff5f7c019336f64d8f96faf812943070d0b
Remco Bouckaert <higgs.ml@gmail.com>
2022-09-06 16:45:47 +1200

    towards a gene clock panel for BEAUti

--- src/starbeast3/app/beauti/StarBeast3ClockInputEditor.java
+++ src/starbeast3/app/beauti/StarBeast3ClockInputEditor.java
@@ -1,22 +1,144 @@
 package starbeast3.app.beauti;
 
+
+
+
+import java.util.List;
+
+
 import beast.base.core.BEASTInterface;
+import beast.base.core.Function;
 import beast.base.core.Input;
+import beast.base.inference.StateNode;
+import beastfx.app.inputeditor.BeautiDoc;
 import beastfx.app.inputeditor.ListInputEditor;
+import beastfx.app.inputeditor.AlignmentListInputEditor.Partition0;
+import beastfx.app.util.FXUtils;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import starbeast3.evolution.branchratemodel.StarBeast3Clock;
 
 public class StarBeast3ClockInputEditor extends ListInputEditor {
+	final private static String hmcURL = "";
+
+	public StarBeast3ClockInputEditor() {
+	}
+	
+	public StarBeast3ClockInputEditor(BeautiDoc doc) {
+		super(doc);
+	}
+	
+	@Override
+	public Class<?> type() {
+		return List.class;
+	}
 
 	@Override
 	public Class<?> baseType() {
 		return StarBeast3Clock.class;
 	}
 	
+
 	@Override
 	public void init(Input<?> input, BEASTInterface beastObject, int itemNr, ExpandOption isExpandOption,
 			boolean addButtons) {
-		// using default list input editor suppresses addition of drop down box
-		super.init(input, beastObject, itemNr, isExpandOption, addButtons);
+		List list = (List)input.get();
+		
+		pane = FXUtils.newVBox();
+		HBox label = FXUtils.newHBox();
+		label.getChildren().add(new Label("Select which clock partition to estimate"));
+		label.getChildren().add(FXUtils.createHMCButton(hmcURL));
+		pane.getChildren().add(label);
+		TableView<Item> table = new TableView<>();
+
+
+	    CheckBox selecteAllCheckBox = new CheckBox();
+	    selecteAllCheckBox.setOnAction(
+	        event -> {
+	          event.consume();
+	          table.getItems().forEach(
+	        		  item -> item.setSelected(selecteAllCheckBox.isSelected())
+	          );
+	        });
+
+	    table.setEditable(true);
+	    
+	    TableColumn<Item, String> nameCol = new TableColumn<>("Clock model");
+	    nameCol.setPrefWidth(550);
+	    nameCol.setCellValueFactory(data -> data.getValue().nameProperty());
+	    table.getColumns().add(nameCol);
+	    
+	    TableColumn<Item, Boolean> selectedCol = new TableColumn<>();
+	    selectedCol.setGraphic(selecteAllCheckBox);
+	    selectedCol.setSortable(false);
+	    selectedCol.setPrefWidth(50);
+
+	    selectedCol.setCellFactory( tc -> {
+	    	CheckBoxTableCell cell =  new CheckBoxTableCell<>();
+	    	List o = cell.getChildrenUnmodifiable();
+	    	return cell;
+	    });
+	    selectedCol.setCellValueFactory( f -> f.getValue().selectedProperty());
+	    selectedCol.setOnEditCommit(e->{
+			Boolean newValue = e.getNewValue();
+			Item item = (Item) e.getSource();
+			item.setSelected(newValue);
+		});
+	    selectedCol.setEditable(true);
+	    table.getColumns().add(selectedCol);
+
+	    pane.getChildren().add(table);
+	    
+		for(Object o : list) {
+			StarBeast3Clock clockmodel = (StarBeast3Clock) o;
+			table.getItems().add(new Item(clockmodel));
+		}
+		
+	    getChildren().add(pane);
 	}
 
+	
+	public class Item {
+		private StarBeast3Clock clock;
+		
+	    private BooleanProperty selected = new SimpleBooleanProperty();
+	    public void setSelected(Boolean selected) { 
+			System.err.println(name + " => " + selected);
+	    	this.selected.set(selected); 
+			Function f = clock.meanRateInput.get();
+			StateNode s = (StateNode) f;
+			s.isEstimatedInput.setValue(selected, s);
+	    }
+	    public Boolean isSelected() { return selected.get(); }
+	    public BooleanProperty selectedProperty() { return selected; }
+
+	    private StringProperty name = new SimpleStringProperty();
+	    public void setName(String name) { this.name.set(name); }
+	    public String getName() { return name.get(); }
+	    public StringProperty nameProperty() { return name; }
+
+	    public Item(StarBeast3Clock clock) {
+	    	this.clock = clock;
+	    	String id = BeautiDoc.parsePartition(clock.getID());
+	    	setName(id);
+			Function f = clock.meanRateInput.get();
+			if (f instanceof StateNode) {
+				StateNode s = (StateNode) f;
+				setSelected(s.isEstimatedInput.get());
+			}
+			selected.addListener((obs,  wasSelected,  isSelected) -> {
+				setSelected(isSelected);
+			});	
+	    }
+	  }
 }
--- src/starbeast3/app/beauti/StarBeastAlignmentProvider3.java
+++ src/starbeast3/app/beauti/StarBeastAlignmentProvider3.java
@@ -1,23 +1,36 @@
 package starbeast3.app.beauti;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import beast.base.core.BEASTInterface;
+import beast.base.core.Log;
+import beast.base.inference.StateNode;
 import beast.base.inference.operator.kernel.BactrianDeltaExchangeOperator;
 import beastfx.app.inputeditor.BeautiAlignmentProvider;
 import beastfx.app.inputeditor.BeautiDoc;
+import starbeast3.evolution.branchratemodel.StarBeast3Clock;
 
 public class StarBeastAlignmentProvider3 extends BeautiAlignmentProvider {
 
+	
+	
+	
 	@Override
 	public List<BEASTInterface> getAlignments(BeautiDoc doc, File[] files) {
+		if (doc.getPartitions("Partitions").size() == 0) {
+			// initial partition to be added
+			// so set up default mode
+			doc.autoSetClockRate = false;
+			doc.beauti.get_autoSetClockRate().setSelected(false);
+			doc.autoUpdateFixMeanSubstRate = false;
+			initialValueOfAutoSet = doc.beauti.get_autoSetClockRate().isSelected();
+		}
+
 		final List<BEASTInterface> newAlignments = super.getAlignments(doc, files);
 		final int alignmentCount = newAlignments.size();
-
-		doc.autoSetClockRate = true;
-		doc.beauti.get_autoSetClockRate().setSelected(true);
-		doc.autoUpdateFixMeanSubstRate = false;
 
 		System.out.println(String.format("N_ALIGNMENTS = %d", doc.alignments.size()));
 		
@@ -43,4 +56,76 @@ public class StarBeastAlignmentProvider3 extends BeautiAlignmentProvider {
 
 		return newAlignments;
 	}
+	
+	
+	
+    /**
+     * Set clock rate estimate=true initially
+     */
+    private static StarBeast3Clock firstBeautiClock = null;
+    private static Map<StarBeast3Clock, Integer> beautiClocksChecked = new HashMap<>();
+    private static Boolean initialValueOfAutoSet = null;
+
+    public static void estimateGeneClockRates(BeautiDoc doc) {
+    	if (initialValueOfAutoSet == null) {
+        	// Loaded in an old file. get autoSetClockRate value
+    		initialValueOfAutoSet = doc.beauti.get_autoSetClockRate().isSelected();
+    	}
+    	
+    	// Loaded in an old file. Do not overwrite
+    	if (!initialValueOfAutoSet && !doc.beauti.get_autoSetClockRate().isSelected() && firstBeautiClock == null) {
+    		return;
+    	}
+    	
+    	// Reset
+    	if (doc.beauti.get_autoSetClockRate().isSelected()) {
+    		firstBeautiClock = null;
+    		initialValueOfAutoSet = true;
+    		beautiClocksChecked.clear();
+    		return;
+    	}
+
+    	for (String str : doc.pluginmap.keySet()) {
+    		
+    		// Find the StarBeast3Clocks
+    		BEASTInterface obj = doc.pluginmap.get(str);
+    		if (obj instanceof StarBeast3Clock) {
+    			
+    			StarBeast3Clock clock = (StarBeast3Clock)obj;
+    			
+    			/*
+    			// Do not revisit this clock or the users wishes will be overridden
+				if (beautiClocksChecked.containsKey(clock) && beautiClocksChecked.get(clock) > 0) {
+					Log.warning("StarBeast3Clock Skipping " + clock.getID());
+					continue;
+				}
+    			*/
+    			
+    			if (clock.meanRateInput.get() != null && clock.meanRateInput.get() instanceof StateNode) {
+					
+    				StateNode rp = (StateNode)clock.meanRateInput.get();
+    				rp.isEstimatedInput.set(true);
+    				// The first clock is not estimated
+        			if (firstBeautiClock == null || firstBeautiClock == clock) {
+        				firstBeautiClock = clock;
+        				rp.isEstimatedInput.set(false);
+        			}else {
+    					rp.isEstimatedInput.set(true);
+    					Log.warning("StarBeast3Clock estimating " + rp.getID());
+        			}
+        			
+        			int val = 1;
+        			if (beautiClocksChecked.containsKey(clock)) {
+        				val += beautiClocksChecked.get(clock);
+        			}
+        			beautiClocksChecked.put(clock, val);
+				}
+    			
+    		}
+    		
+    	}
+    
+    	
+    }
+
 }
